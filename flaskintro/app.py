13c457a0ded28c318bc539aa534aea1f49bcd56c
jakob22r <92209540+jakob22r@users.noreply.github.com>
2023-04-27 11:11:40 +0200

    Template setup in Flask

---
+++ flaskintro/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+#Using a decorator to bind root URL to a function, i.e. navigating to root page, will execute the 
+#following function, which will load the hello.html page
+@app.route("/")
+def hello_world(name=None):
+    return render_template('index.html', name=name)
+
+
+#Checks if scripts is executed directly from command line
+if __name__ == "__main__":
+    app.run(debug=True)
+
