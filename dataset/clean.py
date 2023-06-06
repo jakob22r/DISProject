cfe0642a503acbcdd3dc52b8913b3e70822ce9e2
EmilyHoff <89389810+EmilyHoff@users.noreply.github.com>
2023-06-06 14:07:01 +0200

    cleaned files

--- dataset/clean.py
+++ dataset/clean.py
@@ -2,10 +2,37 @@ import pandas as pd
 
 df = pd.read_csv("contestants.csv")
 
-selected_columns = ['year', 'to_country','performer', 'song', 'place_final', 'points_final']
-extracted_df = df[selected_columns]
+#artist
+artist_columns = ['performer']
+artist_df = df[artist_columns]
+artist_df = artist_df.drop_duplicates()
+artist_df.to_csv('artists.csv', index=True)
 
-print(extracted_df)  # Display the extracted data
-extracted_df.to_csv('extracted_contestants.csv', index=True)  # Save the extracted data to a new CSV file
+#counties
+countries_colmns = ['to_country']
+countries_df = df[countries_colmns]
+countries_df = countries_df.drop_duplicates()
+countries_df.to_csv('countries.csv', index=True)
+
+#songs
+songs_colmns = ['year','to_country', 'song']
+songs_df = df[songs_colmns]
+songs_df.to_csv('songs.csv', index=True)
 
 
+#oldsong
+PYsongs_colmns = ['year','to_country', 'song', 'place_final', 'points_final']
+PYsongs_df = df[PYsongs_colmns]
+PYsongs_df = PYsongs_df[PYsongs_df['year'] <= 2023]
+PYsongs_df.to_csv('previousYearsSongs.csv', index=True)
+
+#new song
+newsongs_colmns = ['year','to_country', 'song']
+newSongs_df = df[newsongs_colmns]
+PYsongs_df = newSongs_df[newSongs_df['year'] >= 2023]
+PYsongs_df.to_csv('upcommingYearSongs.csv', index=True)
+
+#preforms
+performs_colmns = ['year','performer', 'song']
+performs_df = df[performs_colmns]
+performs_df.to_csv('performs.csv', index=True)
