dff2944a09780d66a588ad2d30de1a3ed20581e7
EmilyHoff <89389810+EmilyHoff@users.noreply.github.com>
2023-06-07 15:42:43 +0200

    semi done with making the .sql files

---
+++ app/votes.sql
@@ -0,0 +1,35 @@
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1000', 'Arcade', 2019, 'Netherlands');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1001', 'Arcade', 2019, 'Netherlands');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1002', 'Soldi', 2019, 'Italy');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1003', 'Soldi', 2019, 'Italy');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1004', 'Soldi', 2019, 'Italy');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1004', 'Scream', 2019, 'Russia');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1005', 'Scream', 2019, 'Russia');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1005', 'Too Late for Love', 2019, 'Sweden');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1006', 'Proud', 2019, 'North Macedonia');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1007', 'Zero Gravity', 2019, 'Australia');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1009', 'Roi', 2019, 'France');
+
+INSERT INTO s.votes(userID, title, year, countryName)
+	VALUES ('user1009', 'Love Is Forever', 2019, 'Denmark');
--- dataset/clean.py
+++ dataset/clean.py
@@ -2,11 +2,17 @@ import pandas as pd
 import re
 import numpy as np
 
+def cleanPerformer(df):
+    df['performer'] = df['performer'].apply(lambda x: re.sub(',', '', str(x)))
+    df['performer'] = df['performer'].apply(lambda x: re.sub("'", '', str(x)))
+    return df
+
+def cleanSong(df):
+    df['song'] = df['song'].apply(lambda x: re.sub(',', '', str(x)))
+    df['song'] = df['song'].apply(lambda x: re.sub("'", '', str(x)))
+    return df
 
 df = pd.read_csv("contestants.csv")
-#df = df.replace('', pd.NA).dropna()
-#df.dropna(inplace=True)
-
 
 df = df.dropna(subset=['song', 'performer', 'place_final', 'points_final'])
 df = df.drop_duplicates()
@@ -23,30 +29,60 @@ df.to_csv("aud.csv", index=False)
 artist_columns = ['performer']
 artist_df = df[artist_columns]
 artist_df = artist_df.drop_duplicates()
-artist_df['performer'] = artist_df['performer'].apply(lambda x: re.sub(',', '', str(x)))
-artist_df.to_csv('artists.csv', index=False)
+artist_df = cleanPerformer(artist_df)
+
+with open('artist.sql', 'w', encoding='utf8') as artistSQL:
+    for row in range(len(artist_df)):
+        sql_statement = f"INSERT INTO s.artists (artistName) VALUES ('{str(artist_df.iloc[row].loc['performer'])}');\n"
+        artistSQL.write(sql_statement)    
+
 
 #countries
 countries_colmns = ['to_country']
 countries_df = df[countries_colmns]
 countries_df = countries_df.drop_duplicates()
-countries_df.to_csv('countries.csv', index=False)
+
+with open('countries.sql', 'w', encoding='utf8') as countriesSQL:
+    for row in range(len(countries_df)):
+        sql_statement = f"INSERT INTO s.countries (countryName) VALUES ('{str(countries_df.iloc[row].loc['to_country'])}');\n"
+        countriesSQL.write(sql_statement) 
+
 
 #songs
 songs_colmns = ['year', 'to_country', 'song']
 songs_df = df[songs_colmns]
 songs_df = songs_df.drop_duplicates()
-songs_df['song'] = songs_df['song'].apply(lambda x: re.sub(',', '', str(x))) #FIX
-songs_df.to_csv('songs.csv', index=False)
+songs_df = cleanSong(songs_df)
+
+
+with open('songs.sql', 'w', encoding='utf8') as songsSQL:
+    for row in range(len(songs_df)):
+        year = songs_df.iloc[row].loc['year']
+        countryName = str(songs_df.iloc[row].loc['to_country'])
+        title = str(songs_df.iloc[row].loc['song'])
+        sql_statement = f"INSERT INTO s.songs (year, countryName, title) VALUES ({year}, '{countryName}', '{title}');\n"
+        songsSQL.write(sql_statement)
+
 
 #oldsong
-PYsongs_colmns = ['year','to_country', 'song', 'place_final', 'points_final']
 PYsongs_colmns = ['song', 'year', 'place_final', 'points_final', 'to_country']
 PYsongs_df = df[PYsongs_colmns]
 PYsongs_df = PYsongs_df.drop_duplicates()
-PYsongs_df['song'] = PYsongs_df['song'].apply(lambda x: re.sub(',', '', str(x))) #FIX
+PYsongs_df = cleanSong(PYsongs_df)
 PYsongs_df = PYsongs_df[PYsongs_df['year'] <= 2018]
-PYsongs_df.to_csv('previousYearsSongs.csv', index=False)
+
+with open('previousyearssongs.sql', 'w', encoding='utf8') as PYsongsSQL:
+    for row in range(len(PYsongs_df)):
+        title = str((PYsongs_df.iloc[row]).loc['song'])
+        year = (PYsongs_df.iloc[row]).loc['year']
+        placingInFinal = PYsongs_df.iloc[row].loc['place_final'] #måske () problem
+        pointsInFinal = PYsongs_df.iloc[row].loc['points_final']
+        countryName = str((PYsongs_df.iloc[row]).loc['to_country'])
+        sql_statement = f"INSERT INTO s.previousyearssongs (title, year, placingInFinal, pointsInFinal, countryName) VALUES ('{title}', {year}, {placingInFinal}, {pointsInFinal}, '{countryName}');\n"
+        PYsongsSQL.write(sql_statement)
+
+
+#PYsongs_df.to_csv('previousYearsSongs.csv', index=False)
 
 #new song
 newsongs_colmns = ['song', 'year','to_country'] 
