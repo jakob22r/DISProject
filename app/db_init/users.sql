130e72ba720989570221652b8b16a05966ba3e00
jakob22r <92209540+jakob22r@users.noreply.github.com>
2023-06-08 14:32:08 +0200

    Hashing

--- app/db_init/users.sql
+++ app/db_init/users.sql
@@ -1,29 +1,32 @@
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('hej1234', 'Emily', 1000);
+	VALUES ('$2b$12$NVo670LmAvkDBI7FeGOB5OkXQ69sDa/n584D5oq3RAde7EVHaZyyW', 'Emily', 1000);
 
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('kodeordet', 'Jakob', 1001);
+	VALUES ('$2b$12$ozNFAZKxl8muCnvapFkpDO.LYpCzxQ.cq1wyU4Xe/fW47ECsmYS12', 'Jakob', 1001);
 	
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('sortKaffe', 'Anders', 1002);
+	VALUES ('$2b$12$XyA02p0TWPr8XlBLhs9LbeQrVkUBTMklP.0Mbk7UqzuDjT81p99ky', 'Anders', 1002);
 
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('rødHvidRose', 'Nana', 1003);
+	VALUES ('$2b$12$8kmVg5n2x1/uGco7d7sms.uXUIlpwmgjrMWH/AujT/lpbf6TFa2YK', 'Nana', 1003);
 	
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('simpleMaskine', 'Mattias', 1004);
+	VALUES ('$2b$12$G/xXukZC1u5awzOEj9RaM.BycTri7EQi9VB4qZqiFS3Hn7I/RgbG6', 'Mattias', 1004);
 
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('123estland', 'Ole', 1005);
+	VALUES ('$2b$12$frgKfZA1WrJ4tW07YlG.r.YbDpaWAYNdmNWGxLClu3n0AjYQ/od/K', 'Ole', 1005);
 	
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('bedsteTA', 'Asbjørn', 1006);
+	VALUES ('$2b$12$mBCNzPbOMoUMTdYrz01Hye5G3TXw3SXdAVUM47A3XM.mYGO5WbW3.', 'Asbjørn', 1006);
 
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('musTændstik34', 'Cirkeline', 1007);
+	VALUES ('$2b$12$WHp53W4WmPOCc9xrJvajl.1TeoYGgqwUGk6Rlv.sPNh.FxAbl5HMe', 'Cirkeline', 1007);
 	
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('franskCanadier', 'Céline Dion', 1008);
+	VALUES ('$2b$12$Y3LJQmZ3DBTF3lzevMpeUOWGMpEr1g54O3rkqVXtaAGIEg8CZFMyO', 'Céline Dion', 1008);
 
 INSERT INTO s.users(password, userName, userID)
-	VALUES ('mrEurovision', 'Johnny Logan', 1009);
+	VALUES ('$2b$12$64vsF5OkyzX9MIyHVzx0vuhoOo/vnbJ9fCamoAdMYQyCMSyhIaFMO', 'Johnny Logan', 1009);
+
+INSERT INTO s.users(password, userName, userID)
+	VALUES ('$2b$12$uoX31b0RThSsNbJaFq1Ble1Xv2.eVV0o67K18LTuauuNhRHgbt896', 'test_usr', 1010);
